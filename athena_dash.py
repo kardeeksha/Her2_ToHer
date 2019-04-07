b886015bd9e7d81f17670e02aa3d5ad3e96bc420
hmsflynn <flynn36@purdue.edu>
2019-04-06 18:00:49 -0700

    Initial push for Her2Her dash

---
+++ athena_dash.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drop Sequence Data Here or ',
+            html.A('Launch Browser')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'solid',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=False
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
